60c2db2808674c270b038bfdd1989561ceef7185
manafon <andahim@gmail.com>
2020-04-02 21:38:22 +0800

    commit strategy pattern (not complete)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # codecave-for-learning
 just a cave for learning programming
+
+Design pattern reference: https://refactoring.guru/design-patterns
---
+++ design pattern/Strategy/Strategy/Strategy/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Context
+    {
+        // maintain a reference to one of strategy objects
+        // should work with all strateies
+        private IStrategy _strategy;
+
+        public Context()
+        { }
+
+        // Constructor that accept a strategy 
+        public Context(IStrategy strategy)
+        {
+            this._strategy = strategy;
+        }
+
+        // settor that allows at runtime
+        public void SetStrategy(IStrategy strategy)
+        {
+            this._strategy = strategy;
+        }
+
+        public void DoSomeBusinessLogic()
+        {
+            Console.WriteLine("Context: Sorting data using the strategy");
+            
+            // call the strategy but dont know its method
+            var result = this._strategy.DoAlgorithm(new List<string> { "a", "b", "c", "d", "e" });
+
+            string resultStr = string.Empty;
+            foreach (var element in result as List<string>)
+            {
+                resultStr += element + ",";
+            }
+
+            Console.WriteLine(resultStr);
+        }
+    }
+
+    // Strategy interface
+    public interface IStrategy
+    {
+        object DoAlgorithm(object data);
+    }
+
+    class ConcreteStrategyA : IStrategy
+    {
+        public object DoAlgorithm(object data)
+        {
+            var list = data as List<string>;
+            list.Sort();
+
+            return list;
+        }
+    }
+
+    class ConcreteStrategyB : IStrategy
+    {
+        public object DoAlgorithm(object data)
+        {
+            var list = data as List<string>;
+            list.Sort();
+            list.Reverse();
+
+            return list;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new Context();
+
+            Console.Write("Client: Strategy is set to normal sorting");
+            context.SetStrategy(new ConcreteStrategyA());
+            context.DoSomeBusinessLogic();
+
+            Console.WriteLine();
+
+
+
+        }
+    }
+}
